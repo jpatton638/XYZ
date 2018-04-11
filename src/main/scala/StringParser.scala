987936bf632aa0e0301a177bf6c4faf9d60b0009
jpatton638 <jpatton638@gmail.com>
2018-04-11 14:49:23 +0100

    Task 1 completed

--- src/main/scala/StringParser.scala
+++ src/main/scala/StringParser.scala
@@ -1,6 +1,16 @@
 class StringParser {
   def parse(input: String): String = {
-    if (input == "x") "y"
-    else "x"
+
+    val processed = input.map {
+                      case 'x' => "y"
+                      case 'y' => "z"
+                      case 'z' => "x"
+                      case _ => "?"
+                    }
+      if (processed.contains("?")) {
+        "Incorrect String"
+      } else {
+        processed.mkString("")
+      }
   }
 }
--- src/test/scala/StringParserSpec.scala
+++ src/test/scala/StringParserSpec.scala
@@ -6,9 +6,27 @@ class StringParserSpec extends WordSpec with MustMatchers {
 
   "StringParser" should {
 
-    "convert 'x' to 'y'" in {
+    "return error string if invalid argument is given" in {
+      parser.parse("r") mustEqual "Incorrect String"
+      parser.parse("r51ud9%") mustEqual "Incorrect String"
+    }
 
+    "convert 'x' to 'y'" in {
       parser.parse("x") mustEqual "y"
     }
+
+    "convert 'y' to 'z'" in {
+      parser.parse("y") mustEqual "z"
+
+    }
+
+    "convert 'z' to 'x'" in {
+      parser.parse("z") mustEqual "x"
+    }
+
+    "handle multiple characters" in {
+      parser.parse("xyz") mustEqual "yzx"
+    }
+
   }
 }
